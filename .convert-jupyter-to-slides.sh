fc04df0b2007740ef63786d6e535cc74320db8a9
Programming-Sai <saiahprog6@gmail.com>
2025-02-11 09:33:12 +0000

    Added hosting config for slides.

--- .convert-jupyter-to-slides.sh
+++ .convert-jupyter-to-slides.sh
@@ -1,4 +1,14 @@
+convert_notebook() {
+    local format=$1
+    local notebook="/Users/mac/Desktop/CryptAnalysis/presentation/Group_6_Project_Assignment.ipynb"
+    
+    if [[ "$format" == "web" ]]; then
+        jupyter nbconvert "$notebook" --to slides --post serve
+    elif [[ "$format" == "pdf" ]]; then
+        jupyter nbconvert "$notebook" --to webpdf 
+    else
+        echo "Invalid format. Use 'web' for HTML or 'pdf' for WebPDF."
+    fi
+}
 
-#!/bin/bash
-jupyter nbconvert '/Users/mac/Desktop/CryptAnalysis/presentation/Group_6_Project_Assignment.ipynb' --to webpdf
-#  --post serve
+convert_notebook "$1"  # Call function with first script argument
